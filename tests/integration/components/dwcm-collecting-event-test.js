c6b19499d41b3c16c5405e194336afe1dbd6bc8c
Carl Claesson <cfclaesson@gmail.com>
2016-02-04 14:05:01 +0200

    Line ending updates

--- tests/integration/components/dwcm-collecting-event-test.js
+++ tests/integration/components/dwcm-collecting-event-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('dwcm-taxonomy', 'Integration | Component | dwcm taxonomy', {
+moduleForComponent('dwcm-collecting-event', 'Integration | Component | dwcm collecting event', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });"
 
-  this.render(hbs`{{dwcm-taxonomy}}`);
+  this.render(hbs`{{dwcm-collecting-event}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:"
   this.render(hbs`
-    {{#dwcm-taxonomy}}
+    {{#dwcm-collecting-event}}
       template block text
-    {{/dwcm-taxonomy}}
+    {{/dwcm-collecting-event}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
