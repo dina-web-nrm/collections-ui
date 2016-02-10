b82851340b6682b37b47649bbc96cf05d9bab096
Carl Claesson <cfclaesson@gmail.com>
2016-02-10 13:12:33 +0200

    Simple list page

--- app/controllers/collectionobject/new.js
+++ app/controllers/collectionobject/new.js
@@ -1,25 +1,49 @@
-/* global $ */
+/* global moment */
+
 import Ember from 'ember';
 import { translationMacro as t } from 'ember-i18n';
 
 export default Ember.Controller.extend({
     i18n: Ember.inject.service(),
-    
+
+    transitionToCollectionObject (collectionObject) {
+        this.transitionToRoute(
+            'collectionobject.view', collectionObject
+        );
+    },
+
     actions: {
-        navigationClick(fieldGroupId) {     
-            $('html, body').animate({
-                scrollTop: $('#field-group-' + fieldGroupId).offset().top - 10
+        navigationClick(fieldGroupId) {
+            Ember.$('html, body').animate({
+                scrollTop: Ember.$('#field-group-' + fieldGroupId).offset().top - 10
             }, 300);
+        },
+        submitForm () {
+            let controller = this;
+
+            // TODO: Validate and save model.
+            this.model.set('timestampCreated', moment().unix());
+
+            this.store.findRecord('agent', 3).then((agent) => {
+                this.model.set('createdByAgentID', agent);
+                this.model.set('name', 'test-create');
+                this.model.save().then((record) => {
+                        controller.transitionToCollectionObject(record);
+                }).catch((reason) => {
+                    console.log(reason);
+                    // Handle any error and present it.
+                });
+            });
         }
     },
-    
+
     entityType: t('definitions.zoological'),
 
     groups: [
         'dwcm-basic-data',
-        'dwcm-taxonomy',
-        'dwcm-preparation',
-        'dwcm-collecting-event'
+        // 'dwcm-taxonomy',
+        // 'dwcm-preparation',
+        // 'dwcm-collecting-event'
     ]
 
 });
--- app/routes/collectionobject.js
+++ app/routes/collectionobject.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    offset: 0,
     model() {
-        return this.store.findAll('collectionobject');
+        return this.store.query(
+            'collectionobject', {
+                offset: this.offset
+            }
+        );
+    },
+
+    actions: {
+        nextPage: function() {
+            this.offset = parseInt(this.store.peekAll('collectionobject').objectAt(
+                this.store.peekAll('collectionobject').get('length') - 1
+            ).get('id')) + 1;
+
+            return this.refresh();
+        }
     }
 });
