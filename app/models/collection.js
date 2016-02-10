b0dc77e8784b5274361902d3363c1d4ac0947f14
Carl Claesson <cfclaesson@gmail.com>
2016-02-10 11:23:13 +0200

    Add all fields to collection and rename name

--- app/models/collection.js
+++ app/models/collection.js
@@ -1,6 +1,23 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    name: DS.attr(),
-    code: DS.attr()
+    collectionName: DS.attr(),
+    code: DS.attr(),
+    collectionId: DS.attr(),
+    userGroupScopeId: DS.attr(),
+    diciplinID: DS.attr(),
+    timestampCreated: DS.attr(),
+    catalogFormatNumName: DS.attr(),
+    timestampModified: DS.attr(),
+    collectionType: DS.attr(),
+    description: DS.attr(),
+    estimatedSize: DS.attr(),
+    guid: DS.attr(),
+    isEmbeddedCollectingEvent: DS.attr(),
+    primaryFocus: DS.attr(),
+    primaryPurpose: DS.attr(),
+    regNumber: DS.attr(),
+    remarks: DS.attr(),
+    disciplineID: DS.attr(),
+    version: DS.attr()
 });
--- app/serializers/collection.js
+++ app/serializers/collection.js
@@ -1,8 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
-    primaryKey: 'collectionId',
-    attrs: {
-        'name': 'collectionName'
-    }
+    primaryKey: 'collectionId'
 });
