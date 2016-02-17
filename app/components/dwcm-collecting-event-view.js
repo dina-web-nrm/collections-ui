3d8bbed150fb8ed6c5ad2d23c38ae381254ed9f0
Carl Claesson <cfclaesson@gmail.com>
2016-02-17 16:40:19 +0200

    Add ember-leaflet component

--- app/components/dwcm-collecting-event-view.js
+++ app/components/dwcm-collecting-event-view.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    /** Map default zoom level. */
+    zoom: 10,
+
+    /** Locality location converted to array. */
+    location: Ember.computed('model.locality.latitude1', 'model.locality.longitude1',  function () {
+        return [
+            this.get('model').get('locality').get('latitude1'),
+            this.get('model').get('locality').get('longitude1')
+        ];
+    })
 });
