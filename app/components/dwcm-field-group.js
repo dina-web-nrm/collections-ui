5498538e2d2c6cf299c8411b60ed8473babbcd03
Carl Claesson <cfclaesson@gmail.com>
2016-02-26 15:33:44 +0200

    Add docstrings

--- app/components/dwcm-field-group.js
+++ app/components/dwcm-field-group.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    /** Set if field group is collapsed. */
     collapsed: false,
+
+    /** Set to display count in header*/
+    counter: null,
+
     actions: {
+
+        /** Toggle if group is collapsed. */
         toggleCollapse() {
             this.toggleProperty('collapsed');
         }
