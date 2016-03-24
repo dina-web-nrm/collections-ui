8fa3f0820f80025460f1a75b1741df9c226b8e2b
Carl Claesson <cfclaesson@gmail.com>
2016-03-24 13:11:58 +0100

    Specify version of ember-data

--- config/environment.js
+++ config/environment.js
@@ -58,7 +58,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     // api.dina-web is specified in the backend reposity.
-    ENV.HOST = 'https://beta-api.dina-web.net';
+    ENV.HOST = 'http://localhost';
   }
 
   if (environment === 'test') {
