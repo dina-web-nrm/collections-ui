0ab88e7863da5dcd0e0345eeef71f5ace9e2520e
Carl Claesson <cfclaesson@gmail.com>
2016-04-04 17:06:47 +0200

    Enable setting uncertainty radius on locality position

--- app/components/locality-selector.js
+++ app/components/locality-selector.js
@@ -2,6 +2,38 @@ import Ember from 'ember';
 
 import Filterable from '../mixins/filterable';
 
+function metersToRadius(meters, zoomLevel) {
+    let zoomConversion = {
+        0:156412,
+        1:78206,
+        2:39103,
+        3:19551,
+        4:9776,
+        5:4888,
+        6:2444,
+        7:1222,
+        8:610.984,
+        9:305.492,
+        10:152.746,
+        11:76.373,
+        12:38.187,
+        13:19.093,
+        14:9.547,
+        15:4.773,
+        16:2.387,
+        17:1.193,
+        18:0.596,
+        19:0.0005
+    };
+    
+    if (!zoomConversion[zoomLevel]) {
+        console.warn('Cannot convert zoomLevel: ', zoomLevel);
+        return 0;
+    }
+    
+    return meters / zoomConversion[zoomLevel];
+}
+
 export default Ember.Component.extend(Filterable, {
 
     classNames: ['row', 'locality-selector'],
@@ -18,6 +50,11 @@ export default Ember.Component.extend(Filterable, {
     selectedLocality: null,
 
     maximumReached: Ember.computed.gt('localities.length', 199),
+    
+    /** Meters converted to pixels to display on map. */
+    uncertaintyRadiusPixels: Ember.computed('mapZoom', 'newLocality.uncertaintyRadius', function () {
+        return metersToRadius(this.get('newLocality.uncertaintyRadius'), this.get('mapZoom')) * 2;
+    }),
     
     /** Return new locality. */
     newLocality: function () {
