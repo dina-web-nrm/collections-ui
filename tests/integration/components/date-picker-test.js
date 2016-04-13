86805c26f2547c4ce74a1c3abf40a294c7a4213c
Carl Claesson <cfclaesson@gmail.com>
2016-04-13 11:58:50 +0200

    Fix date picker test

--- tests/integration/components/date-picker-test.js
+++ tests/integration/components/date-picker-test.js
@@ -11,14 +11,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{date-picker}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#date-picker}}
-      template block text
-    {{/date-picker}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '-\n\n-');
 });
