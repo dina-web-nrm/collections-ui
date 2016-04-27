9d0d13c8b3b83a741f8cc06c36cfb72c3fe8eb3b
Carl Claesson <cfclaesson@gmail.com>
2016-04-27 16:01:05 +0200

    Fix geography test

--- tests/unit/models/geography-test.js
+++ tests/unit/models/geography-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('geography', 'Unit | Model | geography', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:geography-treedef-item']
 });
 
 test('it exists', function(assert) {
