e9046d15d35f40151bdad61e0e9027e576e429fc
Carl Claesson <cfclaesson@gmail.com>
2016-05-17 13:22:11 +0200

    Remove override and update token base path

--- app/authenticators/oauth.js
+++ app/authenticators/oauth.js
@@ -11,10 +11,7 @@ export default OAuth2PasswordGrantAuthenticator.extend({
     refreshAccessTokens: true,
 
     /** Endpoint for authentication. */
-    serverTokenEndpoint: `${config.AUTHENTICATION_HOST}/auth/realms/dina/tokens/grants/access`,
-
-    /** Endpoint for refreshing token. */
-    serverRefreshTokenEndpoint: `${config.AUTHENTICATION_HOST}/auth/realms/dina/protocol/openid-connect/token`,
+    serverTokenEndpoint: `${config.AUTHENTICATION_HOST}/auth/realms/dina/protocol/openid-connect/token`,
 
     /** Set client id. */
     clientId: 'dina-rest-endpoint',
@@ -28,32 +25,6 @@ export default OAuth2PasswordGrantAuthenticator.extend({
         }
 
         return this._super(...arguments);
-    },
-
-    /** Override to be able to have different endpoint when refreshing a token. */
-    _refreshAccessToken(expiresIn, refreshToken) {
-        const data = { 'grant_type': 'refresh_token', 'refresh_token': refreshToken };
-
-        /* Override starts. */
-        const serverTokenEndpoint = this.get('serverRefreshTokenEndpoint');
-        /* Override ends. */
-
-        return new RSVP.Promise((resolve, reject) => {
-          this.makeRequest(serverTokenEndpoint, data).then((response) => {
-            run(() => {
-              expiresIn = response['expires_in'] || expiresIn;
-              refreshToken = response['refresh_token'] || refreshToken;
-              const expiresAt = this._absolutizeExpirationTime(expiresIn);
-              const data = Ember.merge(response, { 'expires_in': expiresIn, 'expires_at': expiresAt, 'refresh_token': refreshToken });
-              this._scheduleAccessTokenRefresh(expiresIn, null, refreshToken);
-              this.trigger('sessionDataUpdated', data);
-              resolve(data);
-            });
-          }, (xhr, status, error) => {
-            Ember.Logger.warn(`Access token could not be refreshed - server responded with ${error}.`);
-            reject();
-          });
-        });
     },
 
 });
