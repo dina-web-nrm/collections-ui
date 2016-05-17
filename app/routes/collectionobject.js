6d71384b824ae8cf0a440b40088ff63b7a445797
Carl Claesson <cfclaesson@gmail.com>
2016-05-17 11:27:40 +0200

    Enable auth on list and view page

--- app/routes/collectionobject.js
+++ app/routes/collectionobject.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
     /** Configure how to handle query params. */
     queryParams: {
--- app/routes/collectionobject/view.js
+++ app/routes/collectionobject/view.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
     renderTemplate() {
         this.render('collectionobject.view');
     },
