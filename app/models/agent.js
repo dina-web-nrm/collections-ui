97230f3bd84a133f31472f681e61189bf599bb5c
Carl Claesson <cfclaesson@gmail.com>
2016-06-10 09:37:07 +0200

    Add birth and death date to dropdown

--- app/models/agent.js
+++ app/models/agent.js
@@ -4,13 +4,15 @@ import Ember from 'ember';
 export default DS.Model.extend({
     email: DS.attr('string'),
     firstName: DS.attr('string'),
-    guid: DS.attr('string'),
     lastName: DS.attr('string'),
+    dateOfDeath: DS.attr('string'),
+    dateOfBirth: DS.attr('string'),
     fullName: Ember.computed('firstName', 'lastName', function() {
         if (this.get('firstName') && this.get('lastName')) {
             return `${this.get('firstName')} ${this.get('lastName')}`;
         } else {
             return `${this.get('firstName') || this.get('lastName')}`;
         }
-    })
+    }),
+    division: DS.belongsTo('division', {async: true}),
 });
