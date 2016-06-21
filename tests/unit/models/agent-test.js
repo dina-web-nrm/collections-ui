5c27ceeaa3ae4bcdb685e5e119b968eb4960b745
Carl Claesson <cfclaesson@gmail.com>
2016-06-21 09:54:11 +0200

    Fix agent model test

--- tests/unit/models/agent-test.js
+++ tests/unit/models/agent-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('agent', 'Unit | Model | agent', {
     // Specify the other units that are required for this test.
-    needs: ['model:division'],
+    needs: [],
 });
 
 test('it exists', function(assert) {
